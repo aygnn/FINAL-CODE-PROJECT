66e3b599d82650d4629c90a5453e3a722870e72c
aygnn <6yary5x@code.edu.az>
2023-08-17 09:51:55 +0300

    last

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from './Main Page/MainPage';
 import Shopping from './ShopPage/Shop1/Shopping';
 import LoginPage from './Login/LoginPage';
